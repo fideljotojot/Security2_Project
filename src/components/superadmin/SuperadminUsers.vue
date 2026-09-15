cc76492f818cba173f74e8375b7ae3cb841d6f82
fideljotojot <fideljotojot@gmail.com>
2026-09-15 21:01:33 +0800

    fix add new account modal eye icon issue

--- src/components/superadmin/SuperadminUsers.vue
+++ src/components/superadmin/SuperadminUsers.vue
@@ -175,14 +175,14 @@
               <div class="password-input-wrapper">
                 <input :type="showPassword ? 'text' : 'password'" id="password" v-model="form.password" required @input="validatePassword">
                 <button type="button" class="toggle-password" @click="showPassword = !showPassword" :aria-label="showPassword ? 'Hide password' : 'Show password'">
-                  <svg v-if="!showPassword" @click="showPassword = !showPassword" xmlns="http://www.w3.org/2000/svg"
+                  <svg v-if="!showPassword" xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 24 24" fill="currentColor" class="size-6 eye-icon">
                     <path d="M12 15a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z" />
                     <path fill-rule="evenodd"
                       d="M1.323 11.447C2.811 6.976 7.028 3.75 12.001 3.75c4.97 0 9.185 3.223 10.675 7.69.12.362.12.752 0 1.113-1.487 4.471-5.705 7.697-10.677 7.697-4.97 0-9.186-3.223-10.675-7.69a1.762 1.762 0 0 1 0-1.113ZM17.25 12a5.25 5.25 0 1 1-10.5 0 5.25 5.25 0 0 1 10.5 0Z"
                       clip-rule="evenodd" />
                   </svg>
-                  <svg v-else @click="showPassword = !showPassword" xmlns="http://www.w3.org/2000/svg"
+                  <svg v-else xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 24 24" fill="currentColor" class="size-6 eye-icon">
                     <path
                       d="M3.53 2.47a.75.75 0 0 0-1.06 1.06l18 18a.75.75 0 1 0 1.06-1.06l-18-18ZM22.676 12.553a11.249 11.249 0 0 1-2.631 4.31l-3.099-3.099a5.25 5.25 0 0 0-6.71-6.71L7.759 4.577a11.217 11.217 0 0 1 4.242-.827c4.97 0 9.185 3.223 10.675 7.69.12.362.12.752 0 1.113Z" />
